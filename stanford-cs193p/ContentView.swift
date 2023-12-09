c06c00d034b2e6b3483dd12e11ab3ef8ca19bdf3
Yuwen <ylu23@nd.edu>
2023-12-09 16:24:10 -0600

    Initial Commit

---
+++ stanford-cs193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stanford-cs193p
+//
+//  Created by Yuwen Lu on 12/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stanford-cs193p/stanford_cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_cs193pApp.swift
+//  stanford-cs193p
+//
+//  Created by Yuwen Lu on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct stanford_cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
